e1ca0a3cf8c761b44625c5be1e9445774fdf7558
Antti-Ville Tuunainen <avtuunainen@gmail.com>
2026-04-21 00:03:53 +0300

    change socket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,7 +2,7 @@ use rouille::Request;
 use rouille::Response;
 
 fn main() {
-    rouille::start_server("0.0.0.0:80", move |request| {
+    rouille::start_server("0.0.0.0:8081", move |request| {
         Response::text("hello world")
     });
 }
