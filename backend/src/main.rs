77bd338c157e9ec60d536320e5acbdb538e4c8fb
Antti-Ville Tuunainen <avtuunainen@gmail.com>
2026-04-20 23:52:59 +0300

    rouille dependency

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,8 @@
+use rouille::Request;
+use rouille::Response;
+
 fn main() {
-    println!("Hello, world!");
+    rouille::start_server("0.0.0.0:80", move |request| {
+        Response::text("hello world")
+    });
 }
